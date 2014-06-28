a4988c1363eb8e5b246ceb924e72c744cc716d88
Tomasz Posłuszny <tefposluszny@gmail.com>
2014-06-28 23:44:07 +0200

    Fix for static collecting

--- people/settings.py
+++ people/settings.py
@@ -81,5 +81,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/tposluszny/polls/static/'
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
